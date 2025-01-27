503d2d9a9a98bde12f0817c3e9b96b86629d3924
sushma kumari <sushmashahi03@gmail.com>
2025-01-27 21:16:23 +0100

    Your commit message here

--- Variables.py
+++ Variables.py
@@ -1,2 +1,9 @@
 x = "sushma"
 print(x)
+#python keywords can be used for variables in python
+_var1 = 6
+print(_var1)#correct
+#1var = 7#wrong
+#var 1 = 4#wrong gap is not allowed used underscore
+#def=6 wrong because of keyword it is wrong
+#case sensitive
