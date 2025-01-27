e0f55395842c59714be9c1508f69d493f6088e26
sushma kumari <sushmashahi03@gmail.com>
2025-01-27 21:15:13 +0100

    Your commit message here

---
+++ Inputuser.py
@@ -0,0 +1,7 @@
+name= input("Enter the name of the person")
+print("Hello" + name)
+print(type(name))
+#taking input from user
+num= input("Enter the number")
+print(num)
+print(type(num))
---
+++ Typecasting.py
@@ -0,0 +1,15 @@
+num= int((input('Enter the number:')))
+print(num)
+num=num+2
+print(type(num))
+#float
+num= float((input('Enter the number:')))
+print(num)
+num=num+2
+print(type(num))
+#bool
+num= bool((input('Enter the number:')))
+print(num)
+num=num+2
+print(type(num))
+#we just add particular data tpe before input because enter a number is string so it will return string for different datatype do typecasting add that datatype before input
