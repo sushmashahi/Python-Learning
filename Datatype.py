3e46dcf3495f788b7c30e2efc8528d12c7bdcb1a
sushma kumari <30375616+sushmashahi@users.noreply.github.com>
2025-01-27 22:05:28 +0100

    Update Datatype.py

--- Datatype.py
+++ Datatype.py
@@ -3,3 +3,9 @@ a=2
 print(type(a))
 b='sushma'
 print(type(b))
+#mutuable datatype changes after creation list ordered,set unordered,dictinary unordered
+
+
+
+#immutable dta no changes not writable after create speed is high numbers,string,Tuples
+#same kind of the data here int toh int hi h float h toh string , v same ordered easy aceess by index
