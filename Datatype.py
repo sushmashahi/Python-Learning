78793512392fd6d63fb275ace4c0ce24e77e763d
sushma kumari <30375616+sushmashahi@users.noreply.github.com>
2025-04-01 10:24:03 +0200

    Update Datatype.py

--- Datatype.py
+++ Datatype.py
@@ -9,3 +9,14 @@ print(type(b))
 
 #immutable dta no changes not writable after create speed is high numbers,string,Tuples
 #same kind of the data here int toh int hi h float h toh string , v same ordered easy aceess by index
+#integer
+length_a = 10
+#float
+length_b = 1.1233
+#string
+length_a = "sushma"
+#we can write wthout any quote than it will consider string we can use single or double quote
+#Boolean
+a = 5
+print(True)
+# print(true( it will give error because t is small ,same goes with the false because true is not defined kezword and python is case sensitive
