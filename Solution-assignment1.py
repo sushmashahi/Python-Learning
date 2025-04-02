3c5a16a6b0c624562f6d84f316564fb8ccb3d31e
sushma kumari <30375616+sushmashahi@users.noreply.github.com>
2025-04-02 07:34:16 +0200

    Add files via upload

---
+++ Solution-assignment1.py
@@ -0,0 +1,12 @@
+#Q1. Define a variable of all the labours and print the name of each labour.
+#Labour names are:- Mahesh, Mithilesh,Ramesh, Sumesh
+    #labour variable should be something like this 1st_labour, 2nd_labour and so on.
+#note pzthon doesnt satrt with number,so we cant write 1st,2nd,3rd
+
+first_labour = "Mahesh"
+second_labour = "Ramesh"
+third_labour = "Sumesh"
+
+print("The name of first_labour is", first_labour)
+print("The name of second_labour is", second_labour)
+print("The name of third_labour is", third_labour)
